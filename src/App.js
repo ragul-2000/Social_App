4a0f6792e475f5f8a0e7ddad036590471bf7d574
ragul-2000 <ragulmyd2000@gmail.com>
2023-12-12 23:10:09 +0530

    update build for gh-pages

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Register from "./pages/register/Register";
 import {
   createBrowserRouter,
   RouterProvider,
-  Route,
   Outlet,
   Navigate,
 } from "react-router-dom";
@@ -16,16 +15,17 @@ import "./style.scss";
 import { useContext } from "react";
 import { DarkModeContext } from "./context/darkModeContext";
 import { AuthContext } from "./context/authContext";
+import ErrorBoundary from './ErrorBoundary';
 
 function App() {
-
+ 
   const {currentUser} = useContext(AuthContext);
 
   const { darkMode } = useContext(DarkModeContext);
 
   const Layout = () => {
+ 
     return (
-
       <div className={`theme-${darkMode ? "dark" : "light"}`}>
         <Navbar />
         <div style={{ display: "flex" }}>
@@ -77,8 +77,11 @@ function App() {
   ]);
 
   return (
+    
     <div>
+      <ErrorBoundary>
       <RouterProvider router={router} />
+      </ErrorBoundary>
     </div>
   );
 }
